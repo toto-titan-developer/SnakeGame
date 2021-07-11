c97d14e961b526fc917e461bec27f5ab4ef813c8
Wyatt Henderson <henderson.wyatt95@gmail.com>
2021-07-11 10:59:19 -0400

    Initial Commit Version 1.0

--- game.js
+++ game.js
@@ -16,10 +16,10 @@ function main(currenTime){
     if(gameOver) {
         if (window.localStorage.getItem('H-scr') < getScore()){
             window.localStorage.setItem('H-scr', getScore().toString())
-            window.location = '/';
+            window.location = 'https://toto-titan-developer.github.io/SnakeGame/';
         }
 
-        window.location = '/';
+        window.location = 'https://toto-titan-developer.github.io/SnakeGame/';
     }
 
 
